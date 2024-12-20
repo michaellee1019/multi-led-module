bb556990682cdbaaec2ae7786f92142cf91f5a71
Vijay Vuyyuru <vijay.vuyyuru@viam.com>
2024-12-20 13:04:36 -0500

    updaet thing

--- src/main.py
+++ src/main.py
@@ -19,18 +19,21 @@ import io
 LOG = logging.getLogger(__name__)
 MESSAGE_CHUNK_SIZE = 32
 
+
 # used to divide a byte string into 32 byte chunks to send over i2c and then put back together on the read side
 def divide_chunks(l, n):
-    
+
     # looping till length l
-    for i in range(0, len(l), n): 
-        yield l[i:i + n]
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
 
 
 class MultiLed(Generic, EasyResource):
     MODEL: ClassVar[Model] = Model(
         ModelFamily("vijayvuyyuru", "multi-led"), "multi-led"
     )
+    
+    bus = None
 
     @classmethod
     def new(
@@ -60,16 +63,24 @@ class MultiLed(Generic, EasyResource):
             Sequence[str]: A list of implicit dependencies
         """
         if "num_strands" not in config.attributes.fields:
-            raise Exception("A num_strands attribute is required for multi led component. Must be an integer. This is the number of led strips")
+            raise Exception(
+                "A num_strands attribute is required for multi led component. Must be an integer. This is the number of led strips"
+            )
 
         if "strand_length" not in config.attributes.fields:
-            raise Exception("A strand_length attribute is required for multi led component. Must be an integer. This is the number of pixels per strip.")
-        
+            raise Exception(
+                "A strand_length attribute is required for multi led component. Must be an integer. This is the number of pixels per strip."
+            )
+
         if "brightness" not in config.attributes.fields:
-            raise Exception("A brightness attribute is required for multi led component component. Must be a float like 0.2 for 20% brightness")
+            raise Exception(
+                "A brightness attribute is required for multi led component component. Must be a float like 0.2 for 20% brightness"
+            )
 
         if "address" not in config.attributes.fields:
-            raise Exception("A address attribute is required for multi led component. It should be of format 0xADDRESS")
+            raise Exception(
+                "A address attribute is required for multi led component. It should be of format 0xADDRESS"
+            )
         return []
 
     def reconfigure(
@@ -89,39 +100,43 @@ class MultiLed(Generic, EasyResource):
         address = int(address_hex_string, 16)
         LOG.info(f"converted address: {address}")
 
-        
         if self.bus is not None:
             self.bus.close()
-        
+
         self.bus = SMBus(1)
         pixel_config = {
             "num_strands": num_strands,
             "strand_length": strand_length,
-            "brightness": brightness
+            "brightness": brightness,
         }
-        
+
         self.num_strands = num_strands
         self.strand_length = self.strand_length
         self.brightness = brightness
         self.address = address
-        
+
         self.send_message(pixel_config)
-        
+
     async def do_command(
-            self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None,**kwargs,
+        self,
+        command: Mapping[str, ValueTypes],
+        *,
+        timeout: Optional[float] = None,
+        **kwargs,
     ) -> Mapping[str, ValueTypes]:
         LOG.info("do command tings")
         LOG.info(f"value passed into do command: {command}")
-        
+
     def send_message(self, message):
         byte_string = json.dumps(message).encode("utf-8")
         chunks = divide_chunks(byte_string, MESSAGE_CHUNK_SIZE)
         for chunk in chunks:
             self.bus.write_i2c_block_data(self.address, 0x00, chunk)
         LOG.info("sent message over i2c")
-    
+
     async def close(self):
         self.bus.close()
+
 
 if __name__ == "__main__":
     asyncio.run(Module.run_from_registry())
